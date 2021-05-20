e47a53489b32883be572e6698a90f287f801b7fb
Paulus Schoutsen <balloob@gmail.com>
2021-05-20 14:56:20 +0000

    Use reactive element for launch button

--- src/launch-button.ts
+++ src/launch-button.ts
@@ -1,18 +1,26 @@
-import { LitElement, html, PropertyValues } from "lit";
+import { PropertyValues, ReactiveElement } from "@lit/reactive-element";
 
-class LaunchButton extends LitElement {
+class LaunchButton extends ReactiveElement {
   public static isSupported = "bluetooth" in navigator;
   public static noSupportMsg =
     "Your browser does not support bluetooth provisioning. Use Google Chrome or Microsoft Edge.";
 
-  protected render() {
-    return LaunchButton.isSupported
-      ? html`
-          <slot>
-            <button>Connect device to Wi-Fi</button>
-          </slot>
-        `
-      : html`${LaunchButton.noSupportMsg}`;
+  static get properties() {
+    return {
+      hideIfUnsupported: { type: Boolean, attribute: "hide-if-unsupported" },
+    };
+  }
+
+  public hideIfUnsupported = false;
+
+  protected update(changedProps: PropertyValues) {
+    super.update(changedProps);
+
+    this.renderRoot.innerHTML = LaunchButton.isSupported
+      ? "<slot><button>Connect device to Wi-Fi</button></slot>"
+      : this.hideIfUnsupported
+      ? ""
+      : LaunchButton.noSupportMsg;
   }
 
   protected firstUpdated(changedProps: PropertyValues) {
