42a2401b20c821f326c5193b5de64ded7993156d
Paulus Schoutsen <balloob@gmail.com>
2021-05-20 15:20:02 +0000

    Drop Reactive Element for vanilla WC

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # JavaScript SDK for Improv Wi-Fi
+
+## Installation
+
+You can use the JavaScript SDK by adding the following HTML to your website:
+
+```html
+<script type="module" src="https://www.improv-wifi.com/sdk-js/launch-button.js"></script>
+```
+
+If you are using a bundler and JavaScript package manager, you can install the SDK via NPM:
+
+```
+npm install --save improv-wifi-sdk
+```
+
+And then import it in your code:
+
+```
+import 'improv-wifi-sdk';
+```
+
+## Usage
+
+Add the following to your website to show a button that start the provisioning process:
+
+```html
+<improv-wifi-launch-button></improv-wifi-launch-button>
+```
+
+This will render a button if it is supported and a warning message if it is not.
+
+It is possible to customize the button and the message. You do this by putting your elements inside the `<improv-wifi-launch-button>` element and adding the appropriate `slot` attribute. Use `activate` to replace the activation button and `unsupported` to replace the unsupported message:
+
+```html
+<improv-wifi-launch-button>
+  <button slot='activate'>Start provision!</button>
+  <span slot='unsupported'>Your browser does not support provisioning.</span>
+</improv-wifi-launch-button>
+```
+
+## Browser Support
+
+This SDK requires a browser with support for WebBluetooth. Currently this is supported by Google Chrome, Microsoft Edge and other browsers based on the Blink engine.
+
+No iOS devices are supported.
--- src/launch-button.ts
+++ src/launch-button.ts
@@ -1,39 +1,40 @@
-import { PropertyValues, ReactiveElement } from "@lit/reactive-element";
-
-class LaunchButton extends ReactiveElement {
+class LaunchButton extends HTMLElement {
   public static isSupported = "bluetooth" in navigator;
-  public static noSupportMsg =
-    "Your browser does not support bluetooth provisioning. Use Google Chrome or Microsoft Edge.";
 
-  static get properties() {
-    return {
-      hideIfUnsupported: { type: Boolean, attribute: "hide-if-unsupported" },
-    };
+  static get observedAttributes() {
+    return ["hide-if-unsupported"];
   }
 
-  public hideIfUnsupported = false;
+  private renderRoot?: ShadowRoot;
 
-  protected update(changedProps: PropertyValues) {
-    super.update(changedProps);
+  public connectedCallback() {
+    this.update();
+  }
+
+  public attributeChangedCallback() {
+    this.update();
+  }
+
+  protected update() {
+    if (!this.renderRoot) {
+      this.renderRoot = this.attachShadow({ mode: "open" });
+
+      if (LaunchButton.isSupported) {
+        this.addEventListener("mouseover", () => {
+          // Preload
+          import("./provision");
+        });
+        this.addEventListener("click", async (ev) => {
+          ev.preventDefault();
+          const mod = await import("./provision");
+          mod.startProvisioning();
+        });
+      }
+    }
 
     this.renderRoot.innerHTML = LaunchButton.isSupported
-      ? "<slot><button>Connect device to Wi-Fi</button></slot>"
-      : this.hideIfUnsupported
-      ? ""
-      : LaunchButton.noSupportMsg;
-  }
-
-  protected firstUpdated(changedProps: PropertyValues) {
-    super.firstUpdated(changedProps);
-    this.addEventListener("mouseover", () => {
-      // Preload
-      import("./provision");
-    });
-    this.addEventListener("click", async (ev) => {
-      ev.preventDefault();
-      const mod = await import("./provision");
-      mod.startProvisioning();
-    });
+      ? "<slot name='activate'><button>Connect device to Wi-Fi</button></slot>"
+      : "<slot name='unsupported'>Your browser does not support bluetooth provisioning. Use Google Chrome or Microsoft Edge.</slot>";
   }
 }
 
