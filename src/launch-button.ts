b15387219a8289924a9a70cd2fea3af953624b23
Bram Kragten <mail@bramkragten.nl>
2021-06-12 15:07:57 +0200

    Add styling to button + check secure context

--- src/launch-button.ts
+++ src/launch-button.ts
@@ -1,6 +1,47 @@
 class LaunchButton extends HTMLElement {
   public static isSupported = "bluetooth" in navigator;
 
+  public static isAllowed = window.isSecureContext;
+
+  private static style = `
+  button {
+    position: relative;
+    cursor: pointer;
+    font-size: 14px;
+    padding: 8px 28px;
+    color: var(--improv-button-text-color, #fff);
+    background-color: var(--improv-button-color, #03a9f4);
+    border: none;
+    border-radius: 4px;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12), 0 1px 5px 0 rgba(0,0,0,.2);
+  }
+  button::before {
+    content: " ";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    opacity: 0.2;
+    border-radius: 4px;
+  }
+  button:hover {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,.14), 0 1px 7px 0 rgba(0,0,0,.12), 0 3px 1px -1px rgba(0,0,0,.2);
+  }
+  button:hover::before {
+    background-color: rgba(255,255,255,.8);
+  }
+  button:focus {
+    outline: none;
+  }
+  button:focus::before {
+    background-color: white;
+  }
+  button:active::before {
+    background-color: grey;
+  }
+`;
+
   private renderRoot?: ShadowRoot;
 
   public connectedCallback() {
@@ -10,21 +51,45 @@ class LaunchButton extends HTMLElement {
 
     this.renderRoot = this.attachShadow({ mode: "open" });
 
-    if (LaunchButton.isSupported) {
-      this.addEventListener("mouseover", () => {
-        // Preload
-        import("./provision");
-      });
-      this.addEventListener("click", async (ev) => {
-        ev.preventDefault();
-        const mod = await import("./provision");
-        mod.startProvisioning();
-      });
+    if (!LaunchButton.isSupported || !LaunchButton.isAllowed) {
+      this.renderRoot.innerHTML = !LaunchButton.isSupported
+        ? "<slot name='unsupported'>Your browser does not support bluetooth provisioning. Use Google Chrome or Microsoft Edge.</slot>"
+        : "<slot name='not-allowed'>You can only use Improv on HTTPS sites or localhost.</slot>";
+      return;
+    }
+    this.addEventListener("mouseover", () => {
+      // Preload
+      import("./provision");
+    });
+
+    const slot = document.createElement("slot");
+    slot.name = "activate";
+    const button = document.createElement("button");
+    button.innerText = "Connect device to Wi-Fi";
+    slot.append(button);
+
+    slot.addEventListener("click", async (ev) => {
+      ev.preventDefault();
+      const mod = await import("./provision");
+      mod.startProvisioning();
+    });
+
+    if (
+      "adoptedStyleSheets" in Document.prototype &&
+      "replaceSync" in CSSStyleSheet.prototype
+    ) {
+      const sheet = new CSSStyleSheet();
+      // @ts-expect-error
+      sheet.replaceSync(LaunchButton.style);
+      // @ts-expect-error
+      this.renderRoot.adoptedStyleSheets = [sheet];
+    } else {
+      const styleSheet = document.createElement("style");
+      styleSheet.innerText = LaunchButton.style;
+      this.renderRoot.append(styleSheet);
     }
 
-    this.renderRoot.innerHTML = LaunchButton.isSupported
-      ? "<slot name='activate'><button>Connect device to Wi-Fi</button></slot>"
-      : "<slot name='unsupported'>Your browser does not support bluetooth provisioning. Use Google Chrome or Microsoft Edge.</slot>";
+    this.renderRoot.append(slot);
   }
 }
 
